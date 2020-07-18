7931b57426364ddf9a49e3c93ee3068bce8438fd
indiarasabarreto <indiara86@gmail.com>
2020-07-18 19:10:22 -0300

    Primeiro Projeto

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', start);
+
+
+
+function start() {
+  var input = document.querySelectorAll("input");
+    for(var i = 0; i < input.length; i++) {
+    input[i].addEventListener("input", function() {
+      var red = document.getElementById("red").value,
+          green = document.getElementById("green").value,
+          blue = document.getElementById("blue").value;
+      var display = document.getElementById("display");
+        display.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
+        var inputText = document.querySelectorAll(".inputText")
+        inputText[0].value = red;
+        inputText[1].value = green;
+        inputText[2].value = blue;
+
+    });
+  }
+
+}
+
+
+
